ceaace84b05c44e95277c45eee9f7b6c4ca2b7cb
Abrahon <mdabrahon926@gmail.com>
2024-02-27 23:59:17 +0600

    add aos animation

--- src/pages/Product/ProductCard.jsx
+++ src/pages/Product/ProductCard.jsx
@@ -1,25 +1,30 @@
+// import Aos from "aos";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+const ProductCard = ({ product }) => {
+    const { _id, name, image, price } = product;
 
-const ProductCard = ({product}) => {
-    const {_id,name, image, price} = product;
+    useEffect(() => {
+        AOS.init({ duration: "3000" });
+    }, [])
     return (
         <div>
-             <div className="container mx-auto">
-           
-           <div className="shadow-2xl p-2  w-full rounded-lg"   >
-              <Link to={`/productdetails/${_id}`}>
-              <div className="object-cover h-72 w-full">
-               <img className="h-full" src={image} alt="image" />
-               </div>
-               <div>
-                   <h1>{name}</h1>
-                   
-                   <p className="text-xl text-blue-600"> <span className="font-bold">${price}</span></p>
+            <div className="container mx-auto">
 
-               </div>
-              </Link>
-           </div>
-       </div>
+                <div className="shadow-2xl p-2  w-full rounded-lg" data-aos="flip-up" >
+                    <Link to={`/productdetails/${_id}`}>
+                        <div className="object-cover h-72 w-full">
+                            <img className="h-full" src={image} alt="image" />
+                        </div>
+                        <div className="mt-2">
+                            <h1>{name}</h1>
+                            <p className="text-xl text-blue-600"> <span className="font-bold">${price}</span></p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
